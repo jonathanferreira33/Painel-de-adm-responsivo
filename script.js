b1a9c43121e8edbc4a7fb1973a6ab2f6bda7fc23
jonathanferreira33 <jhouus33@gmail.com>
2021-04-21 09:27:07 -0300

    Front-end finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglemenu() {
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
